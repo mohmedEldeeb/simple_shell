14d990ba0306681eb1e1e1c95630afe65713f363
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-05-25 14:01:05 +0300

    init

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - entry point simple shell project
+ * @args: the arg count
+ * @arg the arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int args, char **arg)
+{
+	return (0);
+}
